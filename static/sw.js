7bd110fbc662044965b9c979b67b4af96edf7d1c
Sandeep  Yadav <84236760+hisandeepkumar@users.noreply.github.com>
2026-06-23 18:51:35 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'winlauncher-v4';
-const ASSETS = ['/', '/static/style.css', '/static/script.js', '/static/manifest.json', '/pwa-icons/icon-192.png', '/pwa-icons/icon-512.png'];
+const ASSETS = ['/', '/static/style.css', '/static/script.js', '/static/manifest.json', '/static/icon-192.png', '/static/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => { if(key !== CACHE_NAME) return caches.delete(key); }))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
